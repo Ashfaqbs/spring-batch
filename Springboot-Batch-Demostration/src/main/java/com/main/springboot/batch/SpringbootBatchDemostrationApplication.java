13867b04910cb0ab03eae277844be45148f50210
Ashfaqbs <ash001x@gmail.com>
2023-12-23 21:55:18 +0530

    Auto commit on file change

---
+++ Springboot-Batch-Demostration/src/main/java/com/main/springboot/batch/SpringbootBatchDemostrationApplication.java
@@ -0,0 +1,13 @@
+package com.main.springboot.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBatchDemostrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBatchDemostrationApplication.class, args);
+	}
+
+}
---
+++ Springboot-Batch-Demostration/src/test/java/com/main/springboot/batch/SpringbootBatchDemostrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.springboot.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBatchDemostrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
