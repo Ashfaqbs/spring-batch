da491abe41b90e5678242368262e5db6ab7fe306
Ashfaqbs <ash001x@gmail.com>
2023-12-24 13:29:04 +0530

    General Commits

--- Springboot-Batch-Demostration/src/main/java/com/main/springboot/batch/config/BatchConfig.java
+++ Springboot-Batch-Demostration/src/main/java/com/main/springboot/batch/config/BatchConfig.java
@@ -11,6 +11,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
@@ -66,5 +67,10 @@ public class BatchConfig {
 				.delimited().names("productId", "title", "description", "price", "discount").targetType(Product.class)
 				.build();
 
+	}
+	@Bean
+	public ItemProcessor<Product, Product>itemProcessor()
+	{
+		
 	}
 }
