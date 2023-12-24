9bd1a87fa8185a650a63889e452781bb7ba9d37f
Ashfaqbs <ash001x@gmail.com>
2023-12-24 12:30:32 +0530

    General Commits

--- Springboot-Batch-Demostration/src/main/java/com/main/springboot/batch/config/JobCompleteionNotificationImpl.java
+++ Springboot-Batch-Demostration/src/main/java/com/main/springboot/batch/config/JobCompleteionNotificationImpl.java
@@ -7,10 +7,23 @@
 
 package com.main.springboot.batch.config;
 
+import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JobCompleteionNotificationImpl implements JobExecutionListener{
+@Override
+public void afterJob(JobExecution jobExecution) {
+	// TODO Auto-generated method stub
+	JobExecutionListener.super.afterJob(jobExecution);
+}
+
+@Override
+	public void beforeJob(JobExecution jobExecution) {
+		// TODO Auto-generated method stub
+		JobExecutionListener.super.beforeJob(jobExecution);
+	}
+
 
 }
