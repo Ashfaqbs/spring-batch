10a53359f3faac5f497f06cf47a0860785a90849
Ashfaqbs <ash001x@gmail.com>
2023-12-24 14:15:15 +0530

    General Commits

--- Springboot-Batch-Demostration/src/main/resources/schema.sql
+++ Springboot-Batch-Demostration/src/main/resources/schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS Products(
 
-productId int primary key,
+productId varchar (100) primary key,
 title varchar(200),
 description varchar(200),
 price varchar(10),
-discount varchar(2),
+discount varchar(10),
 discounted_price varchar(10) 
 
 );
