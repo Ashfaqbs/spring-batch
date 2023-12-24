609237c78dbbdae9c326ef805dd739a58c7fd1a3
Ashfaqbs <ash001x@gmail.com>
2023-12-24 13:30:17 +0530

    General Commits

--- Springboot-Batch-Demostration/src/main/java/com/main/springboot/batch/config/CustomItemProcessor.java
+++ Springboot-Batch-Demostration/src/main/java/com/main/springboot/batch/config/CustomItemProcessor.java
@@ -15,7 +15,6 @@ public class CustomItemProcessor implements ItemProcessor<Product, Product> {
 
 	@Override
 	public Product process(Product item) throws Exception {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
