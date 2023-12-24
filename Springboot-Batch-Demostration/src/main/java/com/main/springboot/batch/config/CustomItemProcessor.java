ed8a2847252c21835e03f1c6c17d0b65a34a285a
Ashfaqbs <ash001x@gmail.com>
2023-12-24 14:30:38 +0530

    General Commits

--- Springboot-Batch-Demostration/src/main/java/com/main/springboot/batch/config/CustomItemProcessor.java
+++ Springboot-Batch-Demostration/src/main/java/com/main/springboot/batch/config/CustomItemProcessor.java
@@ -24,6 +24,7 @@ public class CustomItemProcessor implements ItemProcessor<Product, Product> {
 			double finalPrice = originalPrice - discount;
 
 			item.setDiscount(String.valueOf(finalPrice));
+			
 		} catch (NumberFormatException e) {
 			// TODO: handle exception
 			e.printStackTrace();
