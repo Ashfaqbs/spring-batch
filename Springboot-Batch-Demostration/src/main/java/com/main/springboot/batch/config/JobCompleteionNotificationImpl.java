abae2e2bec7b40dc88ba4d0951e5bf0f7b4adc8e
Ashfaqbs <ash001x@gmail.com>
2023-12-24 12:35:17 +0530

    General Commits

--- Springboot-Batch-Demostration/src/main/java/com/main/springboot/batch/config/JobCompleteionNotificationImpl.java
+++ Springboot-Batch-Demostration/src/main/java/com/main/springboot/batch/config/JobCompleteionNotificationImpl.java
@@ -7,6 +7,7 @@ package com.main.springboot.batch.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.stereotype.Component;
@@ -27,10 +28,11 @@ public class JobCompleteionNotificationImpl implements JobExecutionListener {
 	@Override
 	public void afterJob(JobExecution jobExecution) {
 
-		if (jobExecution.isRunning()) {
-			LOGGER.info("Job is running");
-		} else {
+		if (jobExecution.getStatus()==BatchStatus.COMPLETED) {
 			LOGGER.info("Job completed");
+			
+		} else {
+			LOGGER.info("Job is running");
 		}
 
 	}
