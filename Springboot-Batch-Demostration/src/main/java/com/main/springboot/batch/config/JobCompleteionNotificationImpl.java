67d2fcdca4e34efb01c546d0d716bc446084c962
Ashfaqbs <ash001x@gmail.com>
2023-12-24 12:31:36 +0530

    General Commits

--- Springboot-Batch-Demostration/src/main/java/com/main/springboot/batch/config/JobCompleteionNotificationImpl.java
+++ Springboot-Batch-Demostration/src/main/java/com/main/springboot/batch/config/JobCompleteionNotificationImpl.java
@@ -3,27 +3,30 @@
  * GitHub: https://github.com/DarkSharkAsh
  */
 
-
-
 package com.main.springboot.batch.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JobCompleteionNotificationImpl implements JobExecutionListener{
-@Override
-public void afterJob(JobExecution jobExecution) {
-	// TODO Auto-generated method stub
-	JobExecutionListener.super.afterJob(jobExecution);
-}
+public class JobCompleteionNotificationImpl implements JobExecutionListener {
 
-@Override
+	private static final Logger LOGGER = LoggerFactory.getLogger(JobCompleteionNotificationImpl.class);
+
+	@Override
+	public void afterJob(JobExecution jobExecution) {
+		// TODO Auto-generated method stub
+		LOGGER.info(String.format(" ", null));
+		JobExecutionListener.super.afterJob(jobExecution);
+	}
+
+	@Override
 	public void beforeJob(JobExecution jobExecution) {
 		// TODO Auto-generated method stub
 		JobExecutionListener.super.beforeJob(jobExecution);
 	}
-
 
 }
