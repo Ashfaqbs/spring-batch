bbaf7ab15b20e4b0d519b629aa638782bff8b8f9
Ashfaqbs <ash001x@gmail.com>
2023-12-24 13:30:00 +0530

    General Commits

---
+++ Springboot-Batch-Demostration/src/main/java/com/main/springboot/batch/config/CustomItemProcessor.java
@@ -0,0 +1,22 @@
+/**
+ * Code developed by Ashfaq (© [Year])
+ * GitHub: https://github.com/DarkSharkAsh
+ */
+
+
+
+package com.main.springboot.batch.config;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.main.springboot.batch.model.Product;
+
+public class CustomItemProcessor implements ItemProcessor<Product, Product> {
+
+	@Override
+	public Product process(Product item) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
